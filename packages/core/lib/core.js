65541a0de48747ef7cba763e6ff2fb1c37a18b85
李思远 <lisiyuan>
2022-05-31 10:35:45 +0800

    feat：add template

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -4,7 +4,7 @@ module.exports = core;
 const utils = require('@sy-minip-cli/utils');
 function core() {
     // TODO
-    console.log('packages core1111112');
+    console.log('packages core');
     utils();
 }
 core();
